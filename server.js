6b9e752601d00eca05db30564d7348f64bdecc58
Prashant Singh <prashant@codeforamerica.org>
2012-07-25 12:05:30 -0700

    Initial add

---
+++ server.js
@@ -0,0 +1,47 @@
+/*jslint node: true */
+'use strict';
+
+var http = require('http');
+var request = require('request');
+
+var staticPrefix = process.env.STATIC_PREFIX;
+var port = process.env.PORT;
+
+var reWeb = /^\/web(.*)/;
+
+http.createServer(function (req, res) {
+  var result = reWeb.exec(req.url);
+  var newUrl;
+  if (result !== null) {
+    newUrl = staticPrefix + result[1];
+    request.get({
+      url: newUrl,
+      followRedirect: false,
+      encoding: null
+    }, function (err, response, body) {
+      if (response.statusCode === 302 ||
+          response.statusCode === 301) {
+        res.statusCode = response.statusCode;
+        // S3 will return 302 Found if it found an index.html and wants to
+        // redirect us to a directory-like URL (i.e. terminal forward slash)
+        res.setHeader('Location', req.url + '/');
+        res.end();
+      } else {
+        res.setHeader('content-type', response.headers['content-type']);
+        res.setHeader('etag', response.headers['etag']);
+        res.write(body);
+        res.end();
+      }
+    });
+  } else if (req.url === '/') {
+    res.statusCode = 302;
+    res.setHeader('Location', req.url + 'web/');
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+}).listen(port, function () {
+  console.log('Listening on ' + port);
+  console.log('Static prefix: ' + staticPrefix);
+});
